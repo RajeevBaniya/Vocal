a474418d0da2fc8b89b744f1fa72b0088a374e4a
RajeevBaniya <baniyarajeev811@gmail.com>
2026-04-14 11:06:49 +0530

    developed shared constants for common configuration values

---
+++ shared/constants/app-constants.ts
@@ -0,0 +1,6 @@
+const APP_CONSTANTS = {
+  appName: "Vocalflow",
+  settingsStoreName: "settings.json"
+} as const;
+
+export { APP_CONSTANTS };
---
+++ shared/constants/default-values.ts
@@ -0,0 +1,6 @@
+const DEFAULT_VALUES = {
+  windowWidth: 1024,
+  windowHeight: 720
+} as const;
+
+export { DEFAULT_VALUES };
